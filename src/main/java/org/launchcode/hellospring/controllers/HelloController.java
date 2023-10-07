02f1fcec081c0e8922c21095649f34ba2b15a275
Christi-Lee <christileecurrier@gmail.com>
2023-10-07 14:59:49 -0500

    created request handler using @PathVariable

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -3,6 +3,7 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -26,6 +27,15 @@ public class HelloController {
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
+
+//    Handles requests of the form /hello/LaunchCode use path parameter
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name)  {
+        return "Hello, " + name + "!";
+    }
+
+
 
 
 }
